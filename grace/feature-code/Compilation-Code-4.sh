7389a74baa480162cc03b68a31bd27ae3889387d
pwoods2 <phoebewoods3@gmail.com>
2024-11-14 13:30:54 -0600

    updated pathway for Compilation Code

--- grace/feature-code/Compilation-Code-4.sh
+++ grace/feature-code/Compilation-Code-4.sh
@@ -7,7 +7,7 @@ read -p "Enter the specific main.c file to use (e.g., 4-Feature-WT-ACK-Test-Main
 read -p "Enter the name of the output binary (e.g., 4-Feat-WT-ACK): " output
 
 # Define the directory where the .c files are located
-source_dir="/home/pwoods2/week15August2024grace/Grace/4-Feature-Testing-Grace/4FeatureCodeTest"  # Replace with the actual directory
+source_dir="/home/pwoods2/week15August2024grace/week15/grace/feature-code"  # Replace with the actual directory
 
 # Find all .c files in the source directory, excluding all files ending with *Main.c
 other_files=$(find "$source_dir" -type f -name "*.c" ! -name "*Main.c")
