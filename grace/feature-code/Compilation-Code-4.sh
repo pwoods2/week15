346b4c384a84184c952be4aa594f54f5bcd523cc
pwoods2 <phoebewoods3@gmail.com>
2024-11-14 13:20:49 -0600

    moving files to feature-code

---
+++ grace/feature-code/Compilation-Code-4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Prompt the user for input of the specific main.c file to use
+read -p "Enter the specific main.c file to use (e.g., 4-Feature-WT-ACK-Test-Main.c): " main_file
+
+# Prompt the user for input of the output binary name
+read -p "Enter the name of the output binary (e.g., 4-Feat-WT-ACK): " output
+
+# Define the directory where the .c files are located
+source_dir="/home/pwoods2/week15August2024grace/Grace/4-Feature-Testing-Grace/4FeatureCodeTest"  # Replace with the actual directory
+
+# Find all .c files in the source directory, excluding all files ending with *Main.c
+other_files=$(find "$source_dir" -type f -name "*.c" ! -name "*Main.c")
+
+# Compile the specified main.c file with other .c files
+gcc -o "$output" "$source_dir/$main_file" $other_files
+
+# Inform the user
+echo "Compiled $main_file with other source files into $output."
+
